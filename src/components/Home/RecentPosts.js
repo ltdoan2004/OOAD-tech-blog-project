f0f4ee1dab5aafd87ac6d51dc97df1d67b28af9d
ltdoan2004 <doanh25032004@gmail.com>
2024-12-06 13:47:20 +0700

    update layout recent post

--- src/components/Home/RecentPosts.js
+++ src/components/Home/RecentPosts.js
@@ -2,14 +2,24 @@ import React from 'react'
 import {sortBlogs} from '@/src/utils'
 import BlogLayoutOne from '../Blog/BlogLayoutOne'
 import BlogLayoutTwo from '../Blog/BlogLayoutTwo'
+import Link from 'next/link'
 const RecentPosts = ({blogs}) => {
     const sortedBlogs = sortBlogs(blogs);
   return (
     <section className='w-full mt-32 px-32 flex flex-col items-center justify-center'>
-        <h2 className='w-full inline-block font-bold capitalize text-4xl my-8'>
+      <div className='flex justify-between w-full'>
+        <h2 className=' inline-block font-bold capitalize text-4xl'>
             Recent Posts
         </h2>
-   
+        <Link href ="/categories/all" className="inline-block font-medium text-accent underline underline-offset-2 text-lg">View all</Link>
+      </div>
+      <div className='grid grid-cols-3 grid-rows-3 gap-16 mt-16'>
+        {
+          sortedBlogs.slice(5, 11).map((blog, index) => {
+            return <article className='col-span-1 row-span-1 relative' key={index}> blog layout 3 </article>
+        })
+        }
+      </div>
     </section>
     
   )
