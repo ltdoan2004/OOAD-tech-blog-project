fe771bd5b538b09e218014f3ebee8bcfecbd1086
Hồ Thiên Trường <146505511+TruongHo22306@users.noreply.github.com>
2024-12-30 18:11:12 +0700

    Update footer

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 'use client';
 import Link from "next/link";
 import Logo from "./Logo";
-import { DribbbleIcon, GithubIcon, LinkedinIcon, MoonIcon, SunIcon, TwitterIcon } from "../Icons";
+import { GithubIcon, MoonIcon, SunIcon } from "../Icons";
 import siteMetadata from "@/src/utils/siteMetaData";
 import { useThemeSwitch } from "../Hooks/useThemeSwitch";
 import { useState } from "react";
@@ -100,26 +100,8 @@ const toggle = () =>{
           )}
         </button>
       </nav>
-      <div className=" hidden sm:flex items-center">
+     <div className=" hidden sm:flex items-center">
         <NotificationIcon />
-        <a
-          href={siteMetadata.linkedin}
-          rel="noopener noreferrer"
-          className="inline-block w-6 h-6 mr-4"
-          aria-label="Reach out to me via LinkedIn"
-          target="_blank"
-        >
-          <LinkedinIcon className="hover:scale-125 transition-all ease duration-200" />
-        </a>
-        <a
-          href={siteMetadata.twitter}
-          rel="noopener noreferrer"
-          className="inline-block w-6 h-6 mr-4"
-          aria-label="Reach out to me via Twitter"
-          target="_blank"
-        >
-          <TwitterIcon className="hover:scale-125 transition-all ease duration-200" />
-        </a>
         <a
           href={siteMetadata.github}
           rel="noopener noreferrer"
@@ -127,16 +109,7 @@ const toggle = () =>{
           aria-label="Check my profile on Github"
           target="_blank"
         >
-          <GithubIcon className="  hover:scale-125 transition-all ease duration-200 dark:fill-light" />
-        </a>
-        <a
-          href={siteMetadata.dribbble}
-          rel="noopener noreferrer"
-          className="inline-block w-6 h-6 mr-4"
-          aria-label="Check my profile on Dribbble"
-          target="_blank"
-        >
-          <DribbbleIcon className="hover:scale-125 transition-all ease duration-200" />
+          <GithubIcon className="hover:scale-125 transition-all ease duration-200 dark:fill-light" />
         </a>
       </div>
     </header>
