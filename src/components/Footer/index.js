27902f724b0337659c3251be5e660648f3beabf9
Hồ Thiên Trường <146505511+TruongHo22306@users.noreply.github.com>
2024-12-30 18:04:48 +0700

    Update footer(index)

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -16,13 +16,13 @@ const Footer = () => {
 
   return (
     <footer className="mt-16 rounded-2xl bg-dark dark:bg-accentDark/90 m-2 sm:m-10 flex flex-col items-center text-light dark:text-dark">
-      <h3 className="mt-16 font-medium dark:font-bold text-center capitalize text-2xl sm:text-3xl lg:text-4xl px-4">
-        Interesting Stories | Updates | Guides
-      </h3>
-      <p className="mt-5 px-4 text-center w-full sm:w-3/5 font-light dark:font-medium text-sm sm:text-base">
-        Subscribe to learn about new technology and updates. Join over 5000+
-        members community to stay up to date with latest news.
-      </p>
+    <h3 className="mt-16 font-medium dark:font-bold text-center capitalize text-2xl sm:text-3xl lg:text-4xl px-4">
+      Stay Ahead with Stories, Updates, and Expert Guides from the Tech World
+    </h3>
+    <p className="mt-5 px-4 text-center w-full sm:w-3/5 font-light dark:font-medium text-sm sm:text-base">
+      Subscribe to learn about new technology and updates. Join over 5000+ members community to stay up to date with latest news.
+    </p>
+  
 
       <form
         onSubmit={handleSubmit(onSubmit)}
@@ -39,61 +39,49 @@ const Footer = () => {
           type="submit"
           className="bg-dark text-light dark:text-dark dark:bg-light cursor-pointer font-medium rounded px-3 sm:px-5 py-1"
         />
-      </form>
-      <div className="flex items-center mt-8">
-        <a
-          href={siteMetadata.linkedin}
-          className="inline-block w-6 h-6 mr-4"
-          aria-label="Reach out to me via LinkedIn"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <LinkedinIcon className="hover:scale-125 transition-all ease duration-200" />
-        </a>
-        <a
-          href={siteMetadata.twitter}
-          className="inline-block w-6 h-6 mr-4"
-          aria-label="Reach out to me via Twitter"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <TwitterIcon className="hover:scale-125 transition-all ease duration-200" />
-        </a>
-        <a
-          href={siteMetadata.github}
-          className="inline-block w-6 h-6 mr-4 fill-light"
-          aria-label="Check my profile on Github"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <GithubIcon className="fill-light dark:fill-dark  hover:scale-125 transition-all ease duration-200" />
-        </a>
-        <a
-          href={siteMetadata.dribbble}
-          className="inline-block w-6 h-6 mr-4"
-          aria-label="Check my profile on Dribbble"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <DribbbleIcon className="hover:scale-125 transition-all ease duration-200" />
-        </a>
-      </div>
+     </form>
+  
+    <div className="flex items-center mt-8">
+      <a
+        href={siteMetadata.github} // Chỉ giữ lại liên kết đến GitHub
+        className="inline-block w-6 h-6 mr-4 fill-light"
+        aria-label="Check my profile on Github"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <GithubIcon className="fill-light dark:fill-dark hover:scale-125 transition-all ease duration-200" />
+      </a>
+    </div>
+  
+    <p className="mt-4 text-center text-sm sm:text-base font-medium text-light dark:text-dark">
+      Connect, Learn, Innovate – Your Journey Starts Here!💪🏼
+    </p>
 
-      <div className="w-full  mt-16 md:mt-24 relative font-medium border-t border-solid border-light py-6 px-8 flex  flex-col md:flex-row items-center justify-between">
-        <span className="text-center">
-          &copy;2023 CodeBucks. All rights reserved.
-        </span>
-        <Link
-          href="/sitemap.xml"
-          className="text-center underline my-4 md:my-0"
-        >
-          sitemap.xml
-        </Link>
-        <div className="text-center">
-          Made with &hearts; by{" "}
-          <a href="https://devdreaming.com" className="underline" target="_blank">
-            CodeBucks
-          </a>
+    <div className="mt-4">
+    <img 
+      src="" 
+      alt="image" 
+      className="w-32 h-auto rounded-lg"
+    />
+  </div>
+  
+    <div className="w-full mt-16 md:mt-24 relative font-medium border-t border-solid border-light py-6 px-8 flex flex-col md:flex-row items-center justify-between">
+      <span className="text-center">
+        &copy;2024 TechConnect®. All rights reserved.
+      </span>
+      <div className="text-center">
+        Made with &hearts; by{" "}
+        <a href="https://github.com/ltdoan2004" className="underline" target="_blank" rel="noopener noreferrer">
+          ThienDoanh
+        </a>{" "}
+        ,{" "}
+        <a href="https://github.com/TruongHo22306" className="underline" target="_blank" rel="noopener noreferrer">
+          ThienTruong
+        </a>{" "}
+        and{" "}
+        <a href="https://github.com/NguyenThanhNamIT" className="underline" target="_blank" rel="noopener noreferrer">
+          ThanhNam
+        </a>
         </div>
       </div>
     </footer>
