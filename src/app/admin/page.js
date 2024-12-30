04d4e7e539e30157a36920f6243d80c4144f785f
Thanh Nam <ngthanhnam990@gmail.com>
2024-12-30 21:02:31 +0700

    login

---
+++ src/app/admin/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AdminPage() {
+  return (
+    <section className="flex justify-center items-center min-h-screen bg-gray-100 ">
+        This is the admin page
+
+    </section>
+  )
+}
---
+++ src/app/login/page.js
@@ -0,0 +1,12 @@
+import LoginComponent from '@/src/login/LoginComponent'
+import React from 'react'
+
+export default function LoginPage() {
+  return (
+    <section className="flex justify-center items-center min-h-screen bg-gray-100 ">
+
+    <LoginComponent />
+
+    </section>
+  )
+}
