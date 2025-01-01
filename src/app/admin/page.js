89823d34748c3247fe0b3c29e481d7d198f9cd1e
Thanh Nam <ngthanhnam990@gmail.com>
2025-01-01 18:57:27 +0700

    login and 1 blog

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,10 +1,33 @@
-import React from 'react'
+'use client'
+import {allBlogs} from "contentlayer/generated";
+import React, { useState } from 'react';
+import RecentPostsAdmin from "@/src/components/Admin/RecentPostsAdmin";
 
 export default function AdminPage() {
-  return (
-    <section className="flex justify-center items-center min-h-screen bg-gray-100 ">
-        This is the admin page
+  
+  const [blogs, setBlogs] = useState(allBlogs);
 
-    </section>
-  )
+  // const handleAddBlog = () => {
+  //   const newBlog = {
+  //     title: `New Blog Post ${blogs.length + 1}`,
+  //     slug: `new-blog-${blogs.length + 1}`,
+  //     date: new Date().toISOString(),
+  //     content: "This is a new blog post.",
+  //   };
+
+  //   setBlogs([...blogs, newBlog]);
+  //   console.log("New blog added:", newBlog);
+  //   alert('New blog added successfully!');
+  // };
+
+  return (
+    <main className="flex flex-col items-center justify-center p-5">
+      <RecentPostsAdmin blogs={blogs} />
+    </main>
+  );
 }
+
+
+
+
+
